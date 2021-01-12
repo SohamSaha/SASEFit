268e7e5eabb1c369a1256cb8b96421481e021953
Soham Saha <soham.saha74@gmail.com>
2021-01-12 13:07:12 -0800

    added bot functionality to send message every day at 8 pm

--- bot.py
+++ bot.py
@@ -1,33 +1,36 @@
 import discord
 import os
+from discord.ext.tasks import loop
 from discord.ext import commands
 from datetime import datetime
-import asyncio
 
+#Set the command prefix for any future extensions and declare environment variables and other global variables
 bot = commands.Bot(command_prefix='$')
+emojis = ['\N{THUMBS UP SIGN}', '\N{THUMBS DOWN SIGN}']
+SASEFIT_CHANNEL = os.environ['SASEFIT_CHANNEL_ID']
+SASEFIT_TOKEN = os.environ['SASEFIT_TOKEN']
 
 @bot.event
 async def on_ready():
   print ('Bot is ready')
-  
-@bot.command()
-async def test(ctx):
-  emojis= ['\N{THUMBS UP SIGN}', '\N{THUMBS DOWN SIGN}']
-  message = await ctx.send("DID YOU ACHIEVE YOUR GOALS TODAY?")
-  
-  for emoji in emojis:
-    await message.add_reaction(emoji)
 
-async def checkTime():
-  await bot.wait_until_ready()
-  while True:
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    if (current_time == '04:00:00'):
-      print('it time')
-    else:
-      pass
-    await asyncio.sleep(1) 
+@loop(count=None, seconds=1)  #Will run forever every second
+async def check_calendar():
+  channel = bot.get_channel(SASEFIT_CHANNEL)
 
-bot.loop.create_task(checkTime())
-bot.run(os.environ['SASEFIT_TOKEN'])
+  #Get the date and time from the Heroku system and format it appropriately
+  now = datetime.now()
+  current_time = now.strftime("%H:%M:%S")
+
+  #As the  Heroku system uses the GMT+0 timezone, a 8 hour offset is required
+  if (current_time == '04:00:00'):
+    message = await channel.send('Did you achieve your goals?')
+    for emoji in emojis:
+      await message.add_reaction(emoji)
+
+@check_calendar.before_loop
+async def before_check_calendar():
+    await bot.wait_until_ready()  # Wait until bot is ready.
+
+check_calendar.start()
+bot.run(SASEFIT_TOKEN)
