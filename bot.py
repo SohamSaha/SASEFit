05f4697be1d0ce57a6253a020144d3bbb414abe6
Soham Saha <soham.saha74@gmail.com>
2021-01-11 21:29:07 -0800

    adding threading

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
 import discord
 import os
 from discord.ext import commands
-import commands as logic
+from datetime import datetime
+import threading
 
 client = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready(ctx):
-  await ctx.send('hola')
-  if(logic.checkTime()):
-    await ctx.send('hi')
-  
   print ('Bot is ready')
+
+
+def checkTime():
+  threading.Timer(1, checkTime).start()
+
+  now = datetime.now()
+
+  current_time = now.strftime("%H:%M:%S")
+  # print ("Current Time =", current_time)
+
+  if (current_time == '05:30:00'):
+    print('it time')
+  else:
+    print('iit not time')
 
 client.run(os.environ['SASEFIT_TOKEN'])
