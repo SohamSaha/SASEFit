502911d2886d958aac56e6ad5d8a1824657cafb9
Soham Saha <soham.saha74@gmail.com>
2021-01-15 11:04:19 -0800

    added weekly youtube video functionality

--- bot.py
+++ bot.py
@@ -1,34 +1,40 @@
-import discord
 import os
-from discord.ext.tasks import loop
-from discord.ext import commands
+import discord
+import helper as helper
 from datetime import datetime
+from discord.ext import commands
+from discord.ext.tasks import loop
 
 #Set the command prefix for any future extensions and declare environment variables and other global variables
 bot = commands.Bot(command_prefix='$')
 emojis = ['\N{THUMBS UP SIGN}', '\N{THUMBS DOWN SIGN}']
 SASEFIT_TOKEN = os.environ['SASEFIT_TOKEN']
+youtube_channel = 'UCCgLoMYIyP0U56dEhEL1wXQ'
 
-@bot.event
+@bot.event #Verify the bot is ready
 async def on_ready():
   print ('Bot is ready')
-
+  
 @loop(count=None, seconds=1)  #Will run forever every second
-async def check_calendar():
+async def check_calendar_check_in():
   channel = bot.get_channel(797665110629548052)
 
   #Get the date and time from the Heroku system and format it appropriately
   now = datetime.now()
   current_time = now.strftime("%H:%M:%S")
-  #As the  Heroku system uses the GMT+0 timezone, a 8 hour offset is required
-  if (current_time == '04:00:00'):
-    message = await channel.send('Did you achieve your goals?')
-    for emoji in emojis:
-      await message.add_reaction(emoji)
+  current_day = now.strftime("%A")
 
-@check_calendar.before_loop
-async def before_check_calendar():
+  #As the  Heroku system uses the GMT+0 timezone, a 8 hour offset is required
+  if (current_time == '04:00:00'): #Send a check-in every day at 8 PM PST to hold people accountable
+    check_in_message = await channel.send('Did you achieve your goals?')
+    for emoji in emojis:
+      await check_in_message.add_reaction(emoji)
+  elif (current_time == '17:00:00') and (current_day == 'Sunday'): #Send a new video every Sunday at 9 AM PST to help people out
+    suggestion_message = await channel.send(helper.get_channel_videos(youtube_channel))
+
+@check_calendar_check_in.before_loop
+async def before_check_calendar_check_in():
     await bot.wait_until_ready()  # Wait until bot is ready
 
-check_calendar.start()
+check_calendar_check_in.start()
 bot.run(SASEFIT_TOKEN)
---
+++ helper.py
@@ -0,0 +1,32 @@
+import random
+import os
+from googleapiclient.discovery import build
+
+api_key = os.environ['YOUTUBE_API_KEY']
+youtube = build('youtube','v3', developerKey=api_key) #Build the Youtube object using the developer key from the environment
+
+
+def get_channel_videos(channel_id):
+  videos = []
+  next_page_token=None
+
+  #Loop through all the videos until no 'next page' is found which implies all videos have been found
+  while True:
+    vids = youtube.playlistItems().list(playlistId='UUCgLoMYIyP0U56dEhEL1wXQ', part='snippet',maxResults=1,pageToken=next_page_token).execute()
+
+    #If the word 'workout' or 'Workout' is found in the title, assume it's a workout video and append to the final list
+    if 'workout' in vids['items'][0]['snippet']['title'] or 'Workout' in vids['items'][0]['snippet']['title']:
+      message = 'For this week, the suggested warmup/ workout is: ' + vids['items'][0]['snippet']['title'] + ' found at https://www.youtube.com/watch?v=' + vids['items'][0]['snippet']['resourceId']['videoId']
+      videos+=[message]
+
+    #Get the next page token to keep going and finding all the videos. Stop the loop if the last video is found
+    next_page_token= vids.get('nextPageToken')
+    if next_page_token is None:
+      break
+  
+  #Return one random video
+  videos_length=len(videos)
+  return videos[random.randint(0,videos_length-1)]
+
+def test():
+  return ('1')
